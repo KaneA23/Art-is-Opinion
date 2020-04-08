0bc1d73ca483fc908a88a8caaa286cdf9a97bfe7
s1802879 <58692827+s1802879@users.noreply.github.com>
2020-04-08 13:34:52 +0100

    ui change, sprites, particle system

---
+++ Assets/Sprites/CharacterSprites/animation/WalkScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkScript : MonoBehaviour {
+
+    public GameObject PinkLady;
+
+
+    void start() {
+		Instantiate(PinkLady, new Vector3(120, 24, 170), Quaternion.identity);
+	}
+
+    void FixedUpdate() {
+
+		PinkLady.transform.Translate(-0.8f, 0, 0);
+    }
+}
