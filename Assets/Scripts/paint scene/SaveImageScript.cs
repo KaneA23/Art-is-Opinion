2bf21f5663e22c1a82b88831dd5889b69ba76559
KaneA23 <55718634+KaneA23@users.noreply.github.com>
2020-04-24 22:28:20 +0100

    SaveImageScript and LoadImageScript
    Not tested in program.
    Works in test branch.
    
    Need to make changes for master branch

--- Assets/Scripts/paint scene/SaveImageScript.cs
+++ Assets/Scripts/paint scene/SaveImageScript.cs
@@ -1,7 +1,8 @@
 ﻿/// <summary>
-/// Name:       SaveImageScript.css
-/// Purpose:    To save an image to a folder
-/// Author:     Kane Adams
+/// Name:			SaveImageScript.css
+/// Purpose:		To save an image to a folder
+/// Author:			Kane Adams
+/// Date Created:	06/02/2020
 /// </summary>
 
 // Namespaces
@@ -11,49 +12,81 @@ using System.IO;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SaveImageScript : MonoBehaviour {
-	public RenderTexture SaveTexture;
-	float sceneChangeWait;
+
+public class SaveImageScript : MonoBehaviour
+{
+    int numOfPNGs;
+    string filePath;
+
+    public RenderTexture SaveTexture;
 
 
-	/// <summary>
-	/// Calls the co-routine that saves the image that has been drawn (when save button is clicked)
-	/// </summary>
-	public void Save() {
-		StartCoroutine(CoSave());
-		sceneChangeWait = 2;
-	}
-	
-	/// <summary>
-	/// Once the current frame ends, the SaveCamera's image is saved to Assets folder
-	/// </summary>
-	/// <returns>Waits until the endd of the rendered frame</returns>
-	private IEnumerator CoSave() {
-		yield return new WaitForEndOfFrame();
-		Debug.Log(Application.dataPath + "/SavedImage/savedImage.png");
+    float sceneChangeWait; // Time to wait before scene change, Coral
 
-		var folder = Directory.CreateDirectory(Application.dataPath + "/SavedImage");   // Creates the folder that stores the image
 
-		RenderTexture.active = SaveTexture;
+    // Start is called before the first frame update
+    public void Start()
+    {
+        int numOfPNGs = 1;
+        filePath = Application.dataPath + "/Resources/Images";  // Where the image is to be saved
 
-		var texture2D = new Texture2D(SaveTexture.width, SaveTexture.height);
-		texture2D.ReadPixels(new Rect(0, 0, SaveTexture.width, SaveTexture.height), 0, 0);
-		texture2D.Apply();
+        // Stores the info on what is saved in the filePath to an array
+        DirectoryInfo info = new DirectoryInfo(filePath);
+        FileInfo[] fileInfo = info.GetFiles();
 
-		var saveData = texture2D.EncodeToPNG(); // Turns the image seen in the SaveCamera to a PNG
+        // Goes through each file within the array, if the file is a .png, numOfPNGs increments
+        foreach (FileInfo file in fileInfo)
+        {
+            if (file.Extension == ".png")
+            {
+                numOfPNGs++;
+            }
+        }
+    }
 
-		File.WriteAllBytes(Application.dataPath + "/SavedImage/savedImage.png", saveData);  // Saves the .PNG to the desired directory
-	}
 
-	// coral
+    /// <summary>
+    /// Calls the co-routine that saves the image that has been drawn (when save button is clicked)
+    /// </summary>
+    public void Save()
+    {
+        StartCoroutine(CoSave());
 
-	private void Update() {
-		if (sceneChangeWait > 0) {
-			sceneChangeWait -= Time.deltaTime;
-			if (sceneChangeWait <= 0) {
-				SceneManager.LoadScene("StreetScene");
-			}
-		}
-	}
-	// coral
+        sceneChangeWait = 2;
+    }
+
+
+    /// <summary>
+    /// Once the current frame ends, the SaveCamera's image is saved to Assets folder
+    /// </summary>
+    /// <returns>Waits until the endd of the rendered frame</returns>
+    private IEnumerator CoSave()
+    {
+        yield return new WaitForEndOfFrame();
+
+        RenderTexture.active = SaveTexture;
+
+        Texture2D texture2D = new Texture2D(SaveTexture.width, SaveTexture.height);
+        texture2D.ReadPixels(new Rect(0, 0, SaveTexture.width, SaveTexture.height), 0, 0);
+        texture2D.Apply();
+
+        byte[] saveData = texture2D.EncodeToPNG();  // Turns the image seen in the SaveCamera to a PNG
+
+        File.WriteAllBytes(filePath + "/SavedImage" + numOfPNGs + ".png", saveData);  // Saves the .PNG to the desired directory
+    }
+
+
+    // Coral
+    private void Update()
+    {
+        if (sceneChangeWait > 0)
+        {
+            sceneChangeWait -= Time.deltaTime;
+            if (sceneChangeWait <= 0)
+            {
+                SceneManager.LoadScene("StreetScene");
+            }
+        }
+    }
+    // Coral
 }
---
+++ Assets/Scripts/street scene/LoadImageScript.cs
@@ -0,0 +1,70 @@
+﻿/// <summary>
+/// Name:           LoadImageScript.css
+/// Purpose:        To load a .PNG image from resource folder in assets into a scene
+/// Author:         Kane Adams
+/// Date Created:   22/03/2020
+/// </summary>
+
+// Namespaces
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+
+public class LoadImageScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        int numOfPNGs = 0;  // Used to load the last PNG in the folder (loads "SavedImage" + numOfPNGs)
+        string filePath = Application.dataPath + "/Resources/Images";   // Where the images are saved
+
+        // Stores the info on what is saved in the filePath to an array
+        DirectoryInfo info = new DirectoryInfo(filePath);
+        FileInfo[] fileInfo = info.GetFiles();
+
+        // Goes through each file within he array, if the file extension is a '.png', numOfPNGs increments
+        foreach (FileInfo file in fileInfo)
+        {
+            if (file.Extension == ".png")
+            {
+                numOfPNGs++;
+            }
+        }
+
+        string individualFilePath = "file://" + Application.dataPath + "/Resources/Images/SavedImage" + numOfPNGs + ".png";
+        Texture loadedTexture;
+
+        using (UnityWebRequest uwr = UnityWebRequestTexture.GetTexture(individualFilePath))
+        {
+            yield return uwr.SendWebRequest();
+
+            if (uwr.isNetworkError || uwr.isHttpError)
+            {
+                loadedTexture = null;
+
+                Debug.Log("Error loading texture.");
+                Debug.Log(uwr.error);
+            }
+            else
+            {
+                loadedTexture = DownloadHandlerTexture.GetContent(uwr);
+
+                Debug.Log("Succesfully loaded texture!");
+            }
+
+            RawImage image = GetComponent<RawImage>();
+            image.texture = loadedTexture;
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
