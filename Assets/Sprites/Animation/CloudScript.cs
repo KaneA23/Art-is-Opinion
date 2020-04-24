523074cdc47df96b26b6ff6ce40073d0071396fb
s1802879 <58692827+s1802879@users.noreply.github.com>
2020-04-24 18:32:44 +0100

    animations and ui update

---
+++ Assets/Sprites/Animation/CloudScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudScript : MonoBehaviour
+{
+	float timeWait = 240;
+
+	float direction;
+	public WalkController walkController;
+
+
+	private void Start() {
+		if (transform.position.x < 0) {
+
+			GetComponent<SpriteRenderer>().flipX = false;
+			direction = Random.Range(0.1f, 0.2f);
+
+		}
+		if (transform.position.x < 0) {
+			GetComponent<SpriteRenderer>().flipX = true;
+			direction = Random.Range(-0.1f, -0.2f);
+
+		}
+	}
+
+	private void Update() {
+
+		transform.Translate(direction, 0, 0);
+		Debug.Log(direction + " yesyezayezyutr");
+
+
+		if (timeWait > 0) {
+			timeWait -= Time.deltaTime;
+			if (timeWait <= 0) {
+				Destroy(gameObject);
+
+			}
+		}
+	}
+}
---
+++ Assets/Sprites/Animation/WalkScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkScript : MonoBehaviour {
+
+	float timeWait = 20;
+
+	float direction;
+	/// //////////////////////////////////////////// find the walk controller in the scene 
+
+
+	private void Start() {
+
+		if (transform.position.x < 0) {
+			GetComponent<SpriteRenderer>().flipX = false;
+			direction = 0.8f;
+
+		}
+		if (transform.position.x > 0) {
+			GetComponent<SpriteRenderer>().flipX = true;
+			direction = -0.8f;
+		}
+		GetComponent<SpriteRenderer>().material.color = Random.ColorHSV(0f, 1f, 1f, 0.5f, 0.5f, 1f);
+	}
+
+	private void Update() {
+		
+		transform.Translate(direction, 0, 0);
+
+
+		if (timeWait > 0) {
+			timeWait -= Time.deltaTime;
+			if (timeWait <= 0) {
+				Destroy(gameObject);
+
+			}
+		}
+	}
+}
