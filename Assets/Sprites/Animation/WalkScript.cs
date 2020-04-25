057ffdc61575c52675d2ac71f60a3e7c5a0553a8
KaneA23 <55718634+KaneA23@users.noreply.github.com>
2020-04-26 00:04:37 +0100

    Fixed the walking script

--- Assets/Sprites/Animation/WalkScript.cs
+++ Assets/Sprites/Animation/WalkScript.cs
@@ -8,20 +8,28 @@ public class WalkScript : MonoBehaviour {
 
 	float direction;
 
+	public GameObject prefabChild;
+
+
 	private void Start() {
+		SpriteRenderer parentSpriteRender = GetComponent<SpriteRenderer>();
+		SpriteRenderer childSpriteRender = prefabChild.GetComponent<SpriteRenderer>();
+		
 
 		if (transform.position.x < 0) {
-			GetComponent<SpriteRenderer>().flipX = false;
+			parentSpriteRender.flipX = false;
+			childSpriteRender.flipX = false;
 			/////////////////////////////////////////////////////////////////
 			direction = 0.8f;
 
 		}
 		if (transform.position.x > 0) {
-			GetComponent<SpriteRenderer>().flipX = true;
+			parentSpriteRender.flipX = true;
+			childSpriteRender.flipX = true;
 			//////////////////////////////////////////////////////////
 			direction = -0.8f;
 		}
-		GetComponent<SpriteRenderer>().material.color = Random.ColorHSV(0f, 1f, 1f, 0.5f, 0.5f, 1f);
+		parentSpriteRender.material.color = Random.ColorHSV(0f, 1f, 1f, 0.5f, 0.5f, 1f);
 	}
 
 	private void Update() {
