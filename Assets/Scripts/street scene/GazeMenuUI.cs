0aa4daa11521506f7625ad3f649cdfe73313aa8b
s1809503 <55702706+s1809503@users.noreply.github.com>
2020-04-27 14:23:32 +0100

    draw button gaze aware

--- Assets/Scripts/street scene/GazeMenuUI.cs
+++ Assets/Scripts/street scene/GazeMenuUI.cs
@@ -17,6 +17,9 @@ public class GazeMenuUI : MonoBehaviour
     public GameObject menuUI;
     public GameObject galleryUI;
     public GameObject streetUI;
+    public GameObject image;
+    public GameObject yesNoButton;
+    public GameObject cameraShutterClose;
     Vector3 PlayPos;
     Vector3 DrawPos;
     Vector3 GalleryPos;
@@ -126,6 +129,9 @@ public class GazeMenuUI : MonoBehaviour
 
                 if ((DrawPos.x + DrawXMin) < filteredPoint.x && filteredPoint.x < (DrawPos.x + DrawXMax) && (DrawPos.y + DrawYMin) < filteredPoint.y && filteredPoint.y < (DrawPos.y + DrawYMax) && timeBetweenClicks <= 0)
                 {
+                    image.GetComponent<ChangePerson>().Drawclicked();
+                    yesNoButton.GetComponent<YesScript>().YesButton();
+                    cameraShutterClose.SetActive(true);
                     timeBeforeClick = timeBetweenClicks;
                 }
 
