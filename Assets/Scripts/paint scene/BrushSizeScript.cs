acce056b4a5c92ee9a8abb63c4553d2d97ff937a
s1802879 <58692827+s1802879@users.noreply.github.com>
2020-04-27 16:30:24 +0100

    custom brush size

--- Assets/Scripts/paint scene/BrushSizeScript.cs
+++ Assets/Scripts/paint scene/BrushSizeScript.cs
@@ -17,6 +17,8 @@ namespace unitycoder_MobilePaint
         float maxPreviewSize = 2f;
         float minPreviewSize = 0.2f;
 
+		public int customSize = 3;
+
         [SerializeField]
         GameObject brushPreview;
 
@@ -35,28 +37,32 @@ namespace unitycoder_MobilePaint
             // sets the default for brush preview
             brushPreviewSize = 0.3f;
             brushPreview.transform.localScale = new Vector2(brushPreviewSize, brushPreviewSize);
-        }
+
+			customSize = 3;
+		}
 
 
-        // Changes the size of the brush by 5
-        public void IncreaseBrushSize()
-        {
-            // If the new size is bigger than the maxBrushSize, it won't change size
-            if (currentBrushSize <= maxBrushSize - 5 && brushPreviewSize <= maxPreviewSize)
-            {
-                // Increase brush size
-                currentBrushSize += 5;
-                mobilePaint.SetBrushSize(currentBrushSize);
+		// Changes the size of the brush by 5
+		public void IncreaseBrushSize() {
+			// If the new size is bigger than the maxBrushSize, it won't change size
+			if (currentBrushSize <= maxBrushSize - 5 && brushPreviewSize <= maxPreviewSize) {
+				// Increase brush size
+				currentBrushSize += 5;
+				mobilePaint.SetBrushSize(currentBrushSize);
 
-                // Increase preview
-                brushPreviewSize += 0.05f;
-                brushPreview.transform.localScale = new Vector2(brushPreviewSize, brushPreviewSize);
-            }
-        }
+				// Increase preview
+				brushPreviewSize += 0.05f;
+				brushPreview.transform.localScale = new Vector2(brushPreviewSize, brushPreviewSize);
+
+			}
+			if (customSize > 0) {
+				customSize -= 1;
+			}
+		}
 
 
-        // Changes the size of the brush by -5
-        public void DecreaseBrushSize()
+		// Changes the size of the brush by -5
+		public void DecreaseBrushSize()
         {
             // If the new size is smaller than the minBrushSize, it won't change size
             if (currentBrushSize >= minBrushSize + 5 && brushPreviewSize >= minPreviewSize)
@@ -68,7 +74,13 @@ namespace unitycoder_MobilePaint
                 // Decrease preview
                 brushPreviewSize -= 0.05f;
                 brushPreview.transform.localScale = new Vector2(brushPreviewSize, brushPreviewSize);
-            }
-        }
+
+				customSize += 1;
+
+			}
+			if (customSize < 4) {
+				customSize += 1;
+			}
+		}
     }
 }
